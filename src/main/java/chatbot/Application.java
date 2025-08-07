296f830a73ac06602f3be6a40fe7af5b3090463f
notlikelion <notlikelion@gmail.com>
2025-08-07 11:12:07 +0900

    Config.

--- src/main/java/chatbot/Application.java
+++ src/main/java/chatbot/Application.java
@@ -1,8 +1,12 @@
 package chatbot;
 
 import com.google.genai.Client;
+import com.google.genai.types.Content;
+import com.google.genai.types.GenerateContentConfig;
 import com.google.genai.types.GenerateContentResponse;
+import com.google.genai.types.Part;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
@@ -17,11 +21,20 @@ public class Application {
 
         Scanner sc = new Scanner(System.in);
         while (true) {
+            System.out.print("질문 : ");
             String input = sc.nextLine();
             if (input.equals("종료")) return; // 한줄은 괄호 없어도 된다
+            GenerateContentConfig config = GenerateContentConfig.builder()
+                    .systemInstruction(Content.builder()
+                            .parts(List.of(
+                                    Part.builder()
+                                    .text("100자 이내로 핵심만, 꾸미는 표현 없이.")
+                                    .build()))
+                            .build())
+                    .build();
             GenerateContentResponse response = client.models.generateContent(
                     GeminiModel.gemini_2_0_flash.toString(),
-                    input, null);
+                    input, config);
             String text = response
                     .candidates().get() // optional
                     .get(0)
