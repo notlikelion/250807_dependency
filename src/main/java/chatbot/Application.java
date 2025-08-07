a3bb677775753c7afadecdb9e301b449d7c366ac
notlikelion <notlikelion@gmail.com>
2025-08-07 11:05:27 +0900

    Enum, 반복로직

--- src/main/java/chatbot/Application.java
+++ src/main/java/chatbot/Application.java
@@ -3,6 +3,8 @@ package chatbot;
 import com.google.genai.Client;
 import com.google.genai.types.GenerateContentResponse;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // https://github.com/googleapis/java-genai
@@ -13,13 +15,19 @@ public class Application {
         Client client = Client.builder().apiKey(GEMINI_API_KEY).build();
         // https://ai.google.dev/gemini-api/docs/models?hl=ko
 
-        GenerateContentResponse response = client.models.generateContent("gemini-2.0-flash",
-                "점심 메뉴 추천해주세요", null);
-        String text = response
-                        .candidates().get() // optional
-                        .get(0)
-                        .content().get()
-                        .text();
-        System.out.println(text);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("종료")) return; // 한줄은 괄호 없어도 된다
+            GenerateContentResponse response = client.models.generateContent(
+                    GeminiModel.gemini_2_0_flash.toString(),
+                    input, null);
+            String text = response
+                    .candidates().get() // optional
+                    .get(0)
+                    .content().get()
+                    .text();
+            System.out.println(text);
+        }
     }
 }
---
+++ src/main/java/chatbot/GeminiModel.java
@@ -0,0 +1,15 @@
+package chatbot;
+
+public enum GeminiModel {
+    gemini_2_0_flash("gemini-2.0-flash");
+
+    private final String modelName;
+    GeminiModel(String modelName) {
+        this.modelName = modelName;
+    }
+
+    @Override
+    public String toString() {
+        return modelName;
+    }
+}
